ddca258c043220803ae2a6765269a832b97ee490
Akhilesh <akhile@dr.com>
2014-03-12 18:44:31 +0400

    Updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lechatblue::Application.config.secret_key_base = '19c9aebe23b6a0dfa054c87767193896d18b59a24ea2e0a86661d63ec3674d47cfb21b7570b7a67b3feb86236a02af9e4280b53516ac37af03cb26f79f9a1b1d'
