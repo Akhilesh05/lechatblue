b6946d68d77dbd0dc1bb3b39e987468155001fac
akhile <akhile@dr.com>
2014-03-14 19:28:33 +0400

    Updates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
 	def index
-		@layout_details = {controller: params[:controller], action: params[:action], styles: [params[:action]], scripts: [params[:action]]}
+		@layout_details = {
+				controller: params[:controller],
+				action: params[:action],
+				styles: [params[:action]],
+				scripts: [params[:action]]
+		}
 	end
 end
