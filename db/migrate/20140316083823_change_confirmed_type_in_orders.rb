1edad3817d52391f70f5efcf717703cd1bbdd34d
akhile <akhile@dr.com>
2014-03-17 02:11:17 +0400

    Updates

--- db/migrate/20140316083823_change_confirmed_type_in_orders.rb
+++ db/migrate/20140316083823_change_confirmed_type_in_orders.rb
@@ -1,5 +1,20 @@
 class ChangeConfirmedTypeInOrders < ActiveRecord::Migration
   def change
-	  change_column :orders, :confirmed, :boolean
+	  add_column :orders, :convert_confirmed, :boolean, :default => true
+
+	  # look up the schema's to be able to re-inspect the Project model
+	  # http://apidock.com/rails/ActiveRecord/Base/reset_column_information/class
+	  Order.reset_column_information
+
+	  # loop over the collection
+	  Order.all.each do |p|
+		  p.convert_confirmed = p.confirmed == '1'
+		  p.save
+	  end
+
+	  # remove the older status column
+	  remove_column :orders, :confirmed
+	  # rename the convert_status to status column
+	  rename_column :orders,:convert_confirmed,:confirmed
   end
 end
