9022278bce7bfe12d9ffbb2750332e9ffb3a1ec3
akhile <akhile@dr.com>
2014-03-18 02:17:05 +0400

    Updates

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,14 @@
+require 'clockwork'
+
 class Order < ActiveRecord::Base
-	require 'clockwork'
 	serialize :pizza_size, Array
 	serialize :pizza_id, Array
+
+	after_validation :init
 	after_create :generate_confirmation_code
 
+	validates :name, :pizza_size, :pizza_id, :phone, :address, :city, presence: true
 	validates :phone, length: {is: 8}
-	validates :pizza_size, :pizza_id, :phone, :address, :city, :name, presence: true
 
 	def send_confirmation_code
 		api = Clockwork::API.new 'ed6b6375448b8925b9f2739c10c103e71d78f231'
@@ -14,6 +17,10 @@ class Order < ActiveRecord::Base
 	end
 
 	private
+		def init
+			self.confirmed ||= false
+		end
+
 		def generate_confirmation_code
 			digest = Digest::MD5.hexdigest "+lcb#{self.id}lcb-"
 			conf_code = "#{self.id}-#{digest[0..2]}-#{digest[10..12]}"
