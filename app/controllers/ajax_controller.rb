0935328e7d9ee4b4b59244448f5d5f482c0bdff7
akhile <akhile@dr.com>
2014-04-01 01:49:46 +0400

    Updates

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -1,6 +1,6 @@
 class AjaxController < ApplicationController
 	def get_pizza_details
-		@pizza = Pizza.find params[:pizza_id]
+		@pizza = Pizza.find_by id: params[:pizza_id]   ## did not used Pizza.find since it throws 404 automatically if pizza id not found
 		if @pizza.nil?
 			render json: {:error => "404"}
 		else
